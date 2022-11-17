2dadb0adc1bcd1d122eadb88002bf50a6a3d1297
Bianca Picali <61015059+BiancaPicali@users.noreply.github.com>
2022-11-17 12:26:25 -0300

    1711
    começando a validação dos formulários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,16 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tu',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'ti',
+    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
   }
+
 ];
 @NgModule({
   imports: [
